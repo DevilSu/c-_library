246e69befa27b5b0a5ae84438573b4d157bbd79e
devilsu <swimm1rsu@gmail.com>
2018-12-03 00:15:23 -0500

    20181203

--- unistd_funcs.cpp
+++ unistd_funcs.cpp
@@ -1,21 +1,26 @@
 /*
 	20181202
 	unistd.h library showcase
+	Reference: http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html
+	NEED REVISIT tag -> function not learned yet
 */
 #include <stdio.h>
 
 #include <unistd.h>
 
 #include <errno.h>	// get the error message while function fails -> errno
-#include <string.h>	// convert errno to string -> strerror 
+#include <string.h>	// convert errno to string -> strerror
+#include <limits.h> // useage of PATH_MAX
 
 int main()
 {
 	int tmp;
 
-	bool f_sleep	=0;
-	bool f_access	=0;
-	bool f_alarm	=1;
+	bool f_sleep		=0;	
+	bool f_alarm		=0;
+	bool f_brk			=0;
+	bool f_access		=0;
+	bool f_chdir_getcwd	=1;
 	
 	//sleep
 	if(f_sleep)
@@ -27,6 +32,23 @@ int main()
 			Out: 0 if success, else return the remaining time suppose to be sleep
 		*/
 		sleep(2);
+	}
+
+	//alarm
+	//NEED REVISIT
+	if(f_alarm)
+	{
+		/*
+			unsigned int alarm(unsigned int seconds);
+		*/
+		alarm(1);
+	}
+
+	//brk
+	//NEED REVISIT
+	if(f_brk)
+	{
+
 	}
 
 	//access
@@ -50,17 +72,36 @@ int main()
 		if(access("unistd_funcs.cpp", X_OK)==0)printf("exec OK\n");
 		// a non-exist file
 		tmp=access("lkjasldj.c", F_OK);
-		printf("tmp=%d, lkjasldj.c: %s\n", tmp, strerror(errno));	
+		printf("tmp=%d, lkjasldj.c: %s\n", tmp, strerror(errno));
 	}
-	
-	//alarm
-	if(f_alarm)
+
+	//chdir, getcwd
+	if(f_chdir_getcwd)
 	{
 		/*
-			unsigned int alarm(unsigned int seconds);
-		*/
-		alarm(1);
+			int chdir(const char *path);
+			Def: change current working directory
+			In : string of pathname
+			Out: 0 if True, -1 of False
+			Err: if false, can check errno for detail
+		 */
+		tmp=chdir("/media/devilsu/M/Git/MDs");
+		printf("tmp=%d, chdir: %s\n", tmp, strerror(errno));
+
+		/*
+			char *getcwd(char *buf, size_t size);	
+			Def: get current working directory path
+			In1: a string
+			In2: size of the string
+			Out: the origin string
+			Err: if false, will return null pointer, check errno for detail
+		 */
+		char cwd[PATH_MAX];
+		getcwd(cwd, sizeof(cwd));
+		printf("current working directory: %s\n", cwd);
+
 	}
 	
+
 	return 0;
 }
