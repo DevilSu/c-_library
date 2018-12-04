9ea501453b58a978dcb70bf623358e555e5925be
devilsu <swimm1rsu@gmail.com>
2018-12-04 00:18:57 -0500

    tab to space

--- unistd_funcs.cpp
+++ unistd_funcs.cpp
@@ -1,190 +1,190 @@
 /*
-	20181202
-	unistd.h library showcase
-	Reference: http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html
-	NEED REVISIT tag -> function not learned yet
+    20181202
+    unistd.h library showcase
+    Reference: http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html
+    NEED REVISIT tag -> function not learned yet
 */
 #include <stdio.h>
 
 #include <unistd.h>
 
-#include <errno.h>	// get the error message while function fails -> errno
-#include <string.h>	// convert errno to string -> strerror
+#include <errno.h>  // get the error message while function fails -> errno
+#include <string.h> // convert errno to string -> strerror
 #include <limits.h> // useage of PATH_MAX
-#include <fcntl.h>	// open() is declared here
+#include <fcntl.h>  // open() is declared here
 
 int main()
 {
-	bool f_sleep		=0;	
-	bool f_alarm		=0;
-	bool f_brk			=0;
-	bool f_access		=0;
-	bool f_chdir_getcwd	=0;
-	bool f_chroot		=0;
-	bool f_chown		=0;
-	bool f_close_read	=1;
+    bool f_sleep        =0; 
+    bool f_alarm        =0;
+    bool f_brk          =0;
+    bool f_access       =0;
+    bool f_chdir_getcwd =0;
+    bool f_chroot       =0;
+    bool f_chown        =0;
+    bool f_close_read   =1;
 
-	int tmp, tmp1;
-	int fd; 			// file descriptor
-	char buf[255];		// for read file text
-	char cwd[PATH_MAX];	// for getcwd 
-	
-	// sleep
-	if(f_sleep)
-	{
-		/*
-			unsigned int sleep(unsigned int seconds);
-			Def: sleep n seconds.
-			In : integer
-			Out: 0 if success, else return the remaining time suppose to be sleep
-		*/
-		sleep(2);
-	}
+    int tmp, tmp1;
+    int fd;             // file descriptor
+    char buf[255];      // for read file text
+    char cwd[PATH_MAX]; // for getcwd 
+    
+    // sleep
+    if(f_sleep)
+    {
+        /*
+            unsigned int sleep(unsigned int seconds);
+            Def: sleep n seconds.
+            In : integer
+            Out: 0 if success, else return the remaining time suppose to be sleep
+        */
+        sleep(2);
+    }
 
-	// alarm
-	// NEED REVISIT
-	if(f_alarm)
-	{
-		/*
-			unsigned int alarm(unsigned int seconds);
-		*/
-		alarm(1);
-	}
+    // alarm
+    // NEED REVISIT
+    if(f_alarm)
+    {
+        /*
+            unsigned int alarm(unsigned int seconds);
+        */
+        alarm(1);
+    }
 
-	// brk
-	// NEED REVISIT
-	if(f_brk)
-	{
+    // brk
+    // NEED REVISIT
+    if(f_brk)
+    {
 
-	}
+    }
 
-	// access
-	if(f_access)
-	{
-		/*
-			int access(const char *path, int amode);
-			Def: check file exist, read, write, exec permissions
-			In1: string of filename
-			In2: mode
-					F_OK: existance
-					R_OK: read
-					W_OK: write
-					X_OK: exec
-			Out: 0 if True, -1 of False
-			Err: if false, can check errno for detail
-		*/
-		if(access("unistd_funcs.cpp", F_OK)==0)printf("file exist\n");
-		if(access("unistd_funcs.cpp", R_OK)==0)printf("read OK\n");
-		if(access("unistd_funcs.cpp", W_OK)==0)printf("write OK\n");
-		if(access("unistd_funcs.cpp", X_OK)==0)printf("exec OK\n");
-		// a non-exist file
-		tmp=access("lkjasldj.c", F_OK);
-		printf("tmp=%d, lkjasldj.c: %s\n", tmp, strerror(errno));
-	}
+    // access
+    if(f_access)
+    {
+        /*
+            int access(const char *path, int amode);
+            Def: check file exist, read, write, exec permissions
+            In1: string of filename
+            In2: mode
+                    F_OK: existance
+                    R_OK: read
+                    W_OK: write
+                    X_OK: exec
+            Out: 0 if True, -1 of False
+            Err: if false, can check errno for detail
+        */
+        if(access("unistd_funcs.cpp", F_OK)==0)printf("file exist\n");
+        if(access("unistd_funcs.cpp", R_OK)==0)printf("read OK\n");
+        if(access("unistd_funcs.cpp", W_OK)==0)printf("write OK\n");
+        if(access("unistd_funcs.cpp", X_OK)==0)printf("exec OK\n");
+        // a non-exist file
+        tmp=access("lkjasldj.c", F_OK);
+        printf("tmp=%d, lkjasldj.c: %s\n", tmp, strerror(errno));
+    }
 
-	// chdir, getcwd
-	if(f_chdir_getcwd)
-	{
-		/*
-			int chdir(const char *path);
-			Def: change current working directory
-			In : string of pathname
-			Out: 0 if True, -1 of False
-			Err: if false, can check errno for detail
-		 */
-		tmp=chdir("/media/devilsu/M/Git/MDs");
-		printf("tmp=%d, chdir: %s\n", tmp, strerror(errno));
+    // chdir, getcwd
+    if(f_chdir_getcwd)
+    {
+        /*
+            int chdir(const char *path);
+            Def: change current working directory
+            In : string of pathname
+            Out: 0 if True, -1 of False
+            Err: if false, can check errno for detail
+         */
+        tmp=chdir("/media/devilsu/M/Git/MDs");
+        printf("tmp=%d, chdir: %s\n", tmp, strerror(errno));
 
-		/*
-			char *getcwd(char *buf, size_t size);	
-			Def: get current working directory path
-			In1: a string
-			In2: size of the string
-			Out: the origin string
-			Err: if false, will return null pointer, check errno for detail
-		 */
-		getcwd(cwd, sizeof(cwd));
-		printf("current working directory: %s\n", cwd);
-	}
-	
-	// chroot
-	if(f_chroot)
-	{
-		/*
-			int chroot(const char *path);
-			!!! NEED ROOT PERMISSION !!!
-			Def: change root directory to the string (will search pathname begin with the string)
-			In : a string
-			Out: 0 if True, -1 of False
-			Err: if false, will return -1, check errno for detail
-		 */
-		// get current working directory
-		getcwd(cwd, sizeof(cwd));
-		printf("current working directory: %s\n", cwd);
-		// change root pathname to current working directory
-		tmp=chroot(cwd);
-		printf("tmp=%d, chroot: %s\n", tmp, strerror(errno));
-		// goto "test" folder
-		tmp=chdir("/test");
-		printf("tmp=%d, chdir: %s\n", tmp, strerror(errno));
-		// print current working directory 
-		getcwd(cwd, sizeof(cwd));
-		printf("current working directory: %s\n", cwd);
-	}
+        /*
+            char *getcwd(char *buf, size_t size);   
+            Def: get current working directory path
+            In1: a string
+            In2: size of the string
+            Out: the origin string
+            Err: if false, will return null pointer, check errno for detail
+         */
+        getcwd(cwd, sizeof(cwd));
+        printf("current working directory: %s\n", cwd);
+    }
+    
+    // chroot
+    if(f_chroot)
+    {
+        /*
+            int chroot(const char *path);
+            !!! NEED ROOT PERMISSION !!!
+            Def: change root directory to the string (will search pathname begin with the string)
+            In : a string
+            Out: 0 if True, -1 of False
+            Err: if false, will return -1, check errno for detail
+         */
+        // get current working directory
+        getcwd(cwd, sizeof(cwd));
+        printf("current working directory: %s\n", cwd);
+        // change root pathname to current working directory
+        tmp=chroot(cwd);
+        printf("tmp=%d, chroot: %s\n", tmp, strerror(errno));
+        // goto "test" folder
+        tmp=chdir("/test");
+        printf("tmp=%d, chdir: %s\n", tmp, strerror(errno));
+        // print current working directory 
+        getcwd(cwd, sizeof(cwd));
+        printf("current working directory: %s\n", cwd);
+    }
 
-	// chown
-	// NEED REVISIT
-	if(f_chroot)
-	{
-		/*
-			int chown(const char *path, uid_t owner, gid_t group);
-			!!! NEED ROOT PERMISSION !!!
-			Def: change owner or group of a file (folder?)
-			In1: a string
-			In2: 
-			In3: 
-			Out: 0 if True, -1 of False
-			Err: if false, will return -1, check errno for detail
-		 */
-	}
+    // chown
+    // NEED REVISIT
+    if(f_chroot)
+    {
+        /*
+            int chown(const char *path, uid_t owner, gid_t group);
+            !!! NEED ROOT PERMISSION !!!
+            Def: change owner or group of a file (folder?)
+            In1: a string
+            In2: 
+            In3: 
+            Out: 0 if True, -1 of False
+            Err: if false, will return -1, check errno for detail
+         */
+    }
 
-	// close, read
-	if (f_close_read)
-	{
-		/*
-			int close(int fildes);
-			Def: close a file descriptor
-			In : file descriptor
-			Out: 0 if True, -1 of False
-			Err: if false, will return -1, check errno for detail
-		 */
-		
-		/*
-			open() and close() are not in the same library!!!
-			open  -> fcntl.h
-			close -> unistd.h
-			info of open(): http://pubs.opengroup.org/onlinepubs/7908799/xsh/open.html
-		 */
-		fd=open("README.md", O_RDONLY);
-		printf("tmp=%d, open: %s\n", tmp, strerror(errno));
+    // close, read
+    if (f_close_read)
+    {
+        /*
+            int close(int fildes);
+            Def: close a file descriptor
+            In : file descriptor
+            Out: 0 if True, -1 of False
+            Err: if false, will return -1, check errno for detail
+         */
+        
+        /*
+            open() and close() are not in the same library!!!
+            open  -> fcntl.h
+            close -> unistd.h
+            info of open(): http://pubs.opengroup.org/onlinepubs/7908799/xsh/open.html
+         */
+        fd=open("README.md", O_RDONLY);
+        printf("tmp=%d, open: %s\n", tmp, strerror(errno));
 
-		/*
-			ssize_t read(int fildes, void *buf, size_t nbyte);
-			Def: read form file
-			In1: file descriptor
-			In2: pointer to the string buffer
-			In3: the char length you want to read
-			Out: actual char length being read, -1 if fail
-			Err: if false, will return -1, check errno for detail
-		 */
-		tmp1=read(fd, buf, 100);	// read 100 char
-		buf[tmp1]='\0';
-    	printf("First 100 char in \"README.md\" is:\n");
-    	printf("%s\n", buf);
+        /*
+            ssize_t read(int fildes, void *buf, size_t nbyte);
+            Def: read form file
+            In1: file descriptor
+            In2: pointer to the string buffer
+            In3: the char length you want to read
+            Out: actual char length being read, -1 if fail
+            Err: if false, will return -1, check errno for detail
+         */
+        tmp1=read(fd, buf, 100);    // read 100 char
+        buf[tmp1]='\0';
+        printf("First 100 char in \"README.md\" is:\n");
+        printf("%s\n", buf);
 
-    	// close function
-		tmp=close(fd);
-		printf("tmp=%d, close: %s\n", tmp, strerror(errno));
-	}
-	return 0;
+        // close function
+        tmp=close(fd);
+        printf("tmp=%d, close: %s\n", tmp, strerror(errno));
+    }
+    return 0;
 }
